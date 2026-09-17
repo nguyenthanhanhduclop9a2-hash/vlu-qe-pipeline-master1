88f7eb380fbc9af4ca50a8ff958e625317b60015
Nguyễn Thành Anh Đức <nguyenthanhanhduclop9a2@gmail.com>
2026-09-17 20:17:18 +0700

    Change admin password from '123' to '999'

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ function login(username, password) {
   if (!username || !password) {
     return false; // username hoặc password rỗng
   }
-  if (username === 'admin' && password === '123') {
+  if (username === 'admin' && password === '999') {
     return true;
   }
   return false;
